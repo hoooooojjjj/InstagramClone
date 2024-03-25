f6746bd1e47ddc587ed7fc7d41a1b9bd896a342d
sono <rhj080471i@snu.ac.kr>
2024-03-25 19:55:21 +0900

    create : 이모지 선택기 삽입 시도 1

---
+++ emojiPicker.js
@@ -0,0 +1,11 @@
+// 이모지 선택기 초기화
+const picker = new EmojiMart({
+  // 이모지가 삽입될 입력란 선택자 지정
+  input: "#emojiInput",
+  // 이모지 패널이 나타날 위치 지정
+  position: "top-start",
+  // 이모지 패널의 사이즈 설정
+  size: 240,
+  // 이모지 선택기 테마 설정
+  theme: "auto",
+});
