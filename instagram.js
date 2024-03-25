75912047f1598c620522d9183efede1a5ea6a135
sono <rhj080471i@snu.ac.kr>
2024-03-25 22:07:06 +0900

    create : 댓글 기능에 로컬 스토리지 추가

--- instagram.js
+++ instagram.js
@@ -47,24 +47,20 @@ let commentsList = [];
 let commentId = 0;
 const ul = document.querySelector(".comment-wrapper");
 
-const submitComents = (commentText, deleteComment) => {
+// 실습 4
+const submitComents = (commentObj) => {
   const li = document.createElement("li");
-  li.innerText = commentText;
+  li.innerText = commentObj.text;
+  li.id = commentObj.id;
   li.style.fontSize = "20px";
   li.style.listStyle = "none";
   const span = document.createElement("span");
   const img = document.createElement("img");
-  img.id = commentId;
   img.className = "comment-delete-icon";
-  img.onclick = deleteComment;
   img.src = "./images/close.png";
   img.alt = "comment";
-  const img2 = document.createElement("img");
-  img2.className = "comment-delete-icon";
-  img2.src = "./images/close.png";
-  img2.alt = "comment";
+  img.onclick = deleteComment; // 클릭 이벤트에 deleteComment 함수를 직접 연결
   span.appendChild(img);
-  span.appendChild(img2);
   li.appendChild(span);
   ul.appendChild(li);
 };
@@ -73,23 +69,48 @@ commentsCreateForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const commentText = commentInput.value;
   if (!commentText) return;
-  commentsList.push(commentText);
+  const commentObj = {
+    // 사용자의 입력값을 id와 함께 전달하기 위해 객체를 만듬 -> 각 입력값이 어떤 건지 알기 위해
+    text: commentText,
+    id: Date.now(),
+  };
+  commentsList.push(commentObj);
   commentId = commentsList.length;
-  submitComents(commentText, deleteComment);
+  submitComents(commentObj, deleteComment);
   commentInput.value = "";
+  savedToDos();
 });
 
 // 실습 5
 
 const deleteComment = (event) => {
-  console.log(event);
   const li = event.target.parentNode.parentNode; // 이벤트가 일어난 대상의 부모
   li.remove(); // remove() 함수 -> 요소를 지울 수 있음.
-  commentsList = commentsList.filter(function () {
-    return commentId !== parseInt(li.id);
+  commentsList = commentsList.filter(function (comment) {
+    console.log(comment);
+    return comment.id !== parseInt(li.id);
   }); // 클릭한 버튼의 li의 id를 가진 배열의 요소를 지움.
+  console.log(li);
+  savedToDos();
 };
 
 // 실습6
 const footer = document.querySelector(".footer-message");
 footer.innerText = `Ⓒ ${new Date().getFullYear()} INSTAGRAM FROM META`;
+
+// 로컬스토리지에 댓글 저장
+function savedToDos() {
+  // todo를 localstorage에 배열로 저장
+  localStorage.setItem("comment", JSON.stringify(commentsList));
+}
+
+const savedComment = localStorage.getItem("comment");
+
+if (savedComment !== null) {
+  const parsedComment = JSON.parse(savedComment); // 2. 다시 그 문자열을 배열로 전환
+  parsedComment.forEach(submitComents);
+  commentsList = parsedComment; // 기존 로컬에 있는 요소들을 현재 toDos 배열에 계속해서 추가하기
+  // => 이러면 새로고침할 때마다 toDos 배열은 리셋되지만 로컬에는 남아있기 때문에 그걸 다시 toDos 배열에 넣어서
+  // 가져올 수 있다.
+}
+console.log(commentsList);
