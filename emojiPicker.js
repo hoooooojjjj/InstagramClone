c2ba6f213766d7cc314a418813dfb6e580b31a73
sono <rhj080471i@snu.ac.kr>
2024-03-25 20:28:03 +0900

    create : 이모지 선택기 삽입

--- emojiPicker.js
+++ emojiPicker.js
@@ -1,11 +1,13 @@
-// 이모지 선택기 초기화
-const picker = new EmojiMart({
-  // 이모지가 삽입될 입력란 선택자 지정
-  input: "#emojiInput",
-  // 이모지 패널이 나타날 위치 지정
-  position: "top-start",
-  // 이모지 패널의 사이즈 설정
-  size: 240,
-  // 이모지 선택기 테마 설정
-  theme: "auto",
+const button = document.querySelector("#emoji_btn");
+const picker = new EmojiButton({
+  position: "bottom-start",
+});
+
+button.addEventListener("click", () => {
+  picker.togglePicker(button);
+});
+
+picker.on("emoji", (emoji) => {
+  const text_box = document.querySelector("#message");
+  text_box.value += emoji;
 });
